72373b4907229fabe42d2fa76a16ea1482854583
Diego Fleitas <diego.fleitas68@gmail.com>
2024-01-08 14:18:10 -0300

    Add environment vars & nodemon config

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port = process.env.PORT;
+  await app.listen(port);
+  console.log(
+    `NestJS app running on [${process.env.NODE_ENV}] mode at... http://localhost:${port}`,
+  );
 }
 bootstrap();
