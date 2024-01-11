f2e70cc7c0449b40da68365d4293703e82b36eeb
Diego Fleitas <diego.fleitas68@gmail.com>
2024-01-11 13:48:29 -0300

    fix unit tests

--- src/todo/todo.controller.spec.ts
+++ src/todo/todo.controller.spec.ts
@@ -1,6 +1,17 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TodoController } from './todo.controller';
 import { TodoService } from './todo.service';
+import { UpdateTodoDto } from './dto/update-todo.dto';
+
+const testTodo = {
+  id: '5',
+  task: 'Test Todo service',
+  completed: false,
+};
+const testTodoUpdated = {
+  id: '5',
+  completed: true,
+};
 
 describe('TodoController', () => {
   let controller: TodoController;
@@ -8,7 +19,22 @@ describe('TodoController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [TodoController],
-      providers: [TodoService],
+      providers: [
+        TodoController,
+        {
+          provide: TodoService,
+          // using a factory just because
+          useFactory: () => ({
+            create: jest.fn(() => testTodo),
+            findAll: jest.fn(() => [testTodo]),
+            findOne: jest.fn(() => testTodo),
+            update: jest
+              .fn(() => testTodoUpdated)
+              .mockReturnValue(testTodoUpdated),
+            remove: jest.fn(() => true),
+          }),
+        },
+      ],
     }).compile();
 
     controller = module.get<TodoController>(TodoController);
@@ -16,5 +42,29 @@ describe('TodoController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should create a new todo', async () => {
+    expect(await controller.create(testTodo)).toEqual(testTodo);
+  });
+
+  it('should get the todos', async () => {
+    expect(await controller.findAll()).toEqual([testTodo]);
+  });
+
+  it('should find a todo', async () => {
+    expect(await controller.findOne(testTodo.id)).toEqual(testTodo);
+  });
+
+  it('should update the todo', async () => {
+    const todo = await controller.update(
+      testTodo.id,
+      testTodo as unknown as UpdateTodoDto,
+    );
+    expect(todo).toEqual(testTodoUpdated);
+  });
+
+  it('should remove the todo', async () => {
+    await controller.remove('999');
   });
 });
--- src/todo/todo.service.spec.ts
+++ src/todo/todo.service.spec.ts
@@ -1,18 +1,98 @@
-import { Test, TestingModule } from '@nestjs/testing';
+import { Test } from '@nestjs/testing';
+import { getModelToken } from '@nestjs/sequelize';
 import { TodoService } from './todo.service';
+import { Todo } from './entities/todo.entity';
+
+const testTodo = {
+  id: 5,
+  task: 'Test Todo service',
+  completed: false,
+};
+const testTodoUpdated = {
+  id: 5,
+  task: 'Test Todo service',
+  completed: false,
+};
 
 describe('TodoService', () => {
   let service: TodoService;
+  let model: typeof Todo;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [TodoService],
+    const modRef = await Test.createTestingModule({
+      providers: [
+        TodoService,
+        {
+          provide: getModelToken(Todo),
+          useValue: {
+            create: jest.fn(() => testTodo),
+            findAll: jest.fn(() => [testTodo]),
+            findOne: jest.fn(() => testTodo),
+            update: jest.fn(() => testTodoUpdated),
+            destroy: jest.fn(() => null),
+          },
+        },
+      ],
     }).compile();
-
-    service = module.get<TodoService>(TodoService);
+    service = modRef.get(TodoService);
+    model = modRef.get<typeof Todo>(getModelToken(Todo));
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('create', () => {
+    it('should create a todo', async () => {
+      const result = await service.create({
+        task: testTodo.task,
+        completed: testTodo.completed,
+      });
+      expect(result).toEqual(testTodo);
+    });
+  });
+
+  describe('findAll', () => {
+    it('should return an array of todos', async () => {
+      const result = await service.findAll();
+      expect(result).toEqual([testTodo]);
+    });
+  });
+
+  describe('findOne', () => {
+    it('should return a todo', async () => {
+      const result = await service.findOne(1);
+      expect(result).toEqual(testTodo);
+    });
+    it('should throw an error', () => {
+      model.findOne = jest.fn(() => null);
+      expect(service.findOne(999)).rejects.toThrow(
+        'Todo with ID 999 not found',
+      );
+    });
+    it('should get a single todo', () => {
+      const findSpy = jest.spyOn(model, 'findOne');
+      expect(service.findOne(testTodo.id));
+      expect(findSpy).toHaveBeenCalledWith({ where: { id: testTodo.id } });
+    });
+
+    describe('update', () => {
+      it('should update a todo', async () => {
+        const updateSpy = jest.spyOn(model, 'update');
+        const result = await service.update(testTodo.id, testTodoUpdated);
+        expect(result).toEqual(testTodoUpdated);
+        expect(updateSpy).toHaveBeenCalledWith(testTodoUpdated, {
+          where: { id: testTodo.id },
+        });
+      });
+    });
+
+    describe('remove', () => {
+      it('should remove a todo', async () => {
+        const destroySpy = jest.spyOn(model, 'destroy');
+        await service.remove(testTodo.id);
+        expect(destroySpy).toHaveBeenCalledWith({ where: { id: testTodo.id } });
+      });
+    });
   });
 });
